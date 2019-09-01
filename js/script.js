39ed552fc9947efb171e9126540c9a8ae06393bb
rizkyharahap <44848178+rizkyharahap@users.noreply.github.com>
2019-09-02 04:36:46 +0700

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,11 @@
+// Event Modal
+var modal = document.getElementById('modal-box-email');
+
+// showmodal function
+function modalShow() {
+  modal.style.display = "flex";
+}
+// closemodal function
+function modalClose() {
+  modal.style.display = "none";
+}
